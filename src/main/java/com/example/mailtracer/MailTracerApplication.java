ec20ccd40d3fdbc4550772ebe74885bd58118ae2
Little Carlena <littleca@ob-j7.nsk.21-school.ru>
2024-10-17 10:26:42 +0700

    init

---
+++ src/main/java/com/example/mailtracer/MailTracerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mailtracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailTracerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailTracerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mailtracer/MailTracerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mailtracer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailTracerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
